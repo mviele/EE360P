5d83c0e1c52519ac579da887204499d3b032dbd2
Matt Viele <mviele@utexas.edu>
2017-02-08 14:04:26 -0600

    Added lab2 starter files

---
+++ lab2/Q2/FairReadWriteLock.java
@@ -0,0 +1,24 @@
+//Member 1: Matt Viele
+//EID 1: mtv364
+//
+//Member 2: Robbie Zuazua
+//EID 2: rz
+//
+//Member 3: Royce Li
+//EID 3: rl
+
+public class FairReadWriteLock {
+	
+                        
+	public synchronized void beginRead() {
+	}
+	
+	public synchronized void endRead() {
+	}
+	
+	public synchronized void beginWrite() {
+	}
+	public synchronized void endWrite() {
+	}
+}
+	
