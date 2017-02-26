276eeff6b92c52a15f18949c633770e6f6954b68
Matt Viele <mviele@utexas.edu>
2017-02-26 17:43:23 -0600

    Some implementation of client done

--- lab3/Q3/Client.java
+++ lab3/Q3/Client.java
@@ -1,3 +1,4 @@
+
 /*
  * Group EIDs:
  * mtv364
@@ -5,14 +6,18 @@
  * rl26589
  * 
  */
+import java.net.DatagramSocket;
+import java.net.Socket;
+
 import java.util.Scanner;
 
 public class Client {
-  public static void main (String[] args) {
+
+  public static void main(String[] args) {
     String hostAddress;
     int tcpPort;
     int udpPort;
-
+    
     if (args.length != 3) {
       System.out.println("ERROR: Provide 3 arguments");
       System.out.println("\t(1) <hostAddress>: the address of the server");
@@ -25,18 +30,49 @@ public class Client {
     tcpPort = Integer.parseInt(args[1]);
     udpPort = Integer.parseInt(args[2]);
 
+    boolean mode = false; //false = tcp, true = udp
+
+    try{
+      Socket tcp = new Socket("localhost", tcpPort);
+      DatagramSocket udp = new DatagramSocket(udpPort);
+    }
+    catch(Exception e){
+      e.printStackTrace();
+    }
+    
+
     Scanner sc = new Scanner(System.in);
-    while(sc.hasNextLine()) {
+    while (sc.hasNextLine()) {
       String cmd = sc.nextLine();
       String[] tokens = cmd.split(" ");
 
       if (tokens[0].equals("setmode")) {
-        // TODO: set the mode of communication for sending commands to the server 
-        // and display the name of the protocol that will be used in future
-      }
-      else if (tokens[0].equals("purchase")) {
+        if (tokens[1].equalsIgnoreCase("t")) {
+          mode = false;
+        } else if (tokens[1].equalsIgnoreCase("u")) {
+          mode = true;
+        } else {
+          System.out.println("Invalid command, server mode unchanged.");
+        }
+      } else if (tokens[0].equals("purchase")) {
         // TODO: send appropriate command to the server and display the
         // appropriate responses form the server
+        if(mode){
+          try{
+            udpPurchase(tokens[1], tokens[2], Integer.parseInt(tokens[3]));
+          }
+          catch(Exception e){
+            System.out.println("Invalid command format");
+          }
+        }
+        else{
+          try{
+
+          }
+          catch(Exception e){
+            System.out.println("Invalid command format");
+          }
+        }
       } else if (tokens[0].equals("cancel")) {
         // TODO: send appropriate command to the server and display the
         // appropriate responses form the server
@@ -51,5 +87,36 @@ public class Client {
       }
     }
   }
-}
 
+  private static void tcpPurchase(String username, String item, int value){
+
+  }
+
+  private static void udpPurchase(String username, String item, int value){
+
+  }
+
+  private static void tcpCancel(int orderID){
+
+  }
+
+  private static void udpCancel(int orderID){
+
+  }
+
+  private static void tcpSearch(String username){
+
+  }
+
+  private static void udpSearch(String username){
+
+  }
+
+  private static void tcpList(){
+
+  }
+
+  private static void udpList(){
+
+  }
+}
