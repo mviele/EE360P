4c7a3121eea21cbca0936600b0158dddaa57b2cb
royceli804 <royceli804@hotmail.com>
2017-03-01 17:52:15 -0600

    Finished methods inside TCPServerThread

--- lab3/Q3/TCPServerThread.java
+++ lab3/Q3/TCPServerThread.java
@@ -6,46 +6,50 @@
  * 
  */
 
-import java.io.IOException; 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.DatagramPacket;
 import java.net.Socket;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class TCPServerThread extends Thread {
 
 	Socket client;
+	
 	public TCPServerThread(Socket client){
 		this.client = client;
 	}
+	
 	public void run() {
-		Scanner sc1 = null;
 		try{
-			sc1 = new Scanner(client.getInputStream());
+			String returnString = ""; 
+			InputStreamReader input = new InputStreamReader(client.getInputStream());
+			BufferedReader din = new BufferedReader(input); 
 			PrintWriter print = new PrintWriter(client.getOutputStream());
-			String command = sc1.nextLine();
-			Scanner sc2 = new Scanner(command);
-			String keyword = sc2.next();
-			if(keyword.equals("setmode")){
-				
+			String command = din.readLine();
+			StringTokenizer st = new StringTokenizer(command); 
+			String[] tokens = command.split(" ");
+			if(tokens[0].equals("purchase")){
+				returnString = Server.purchase(tokens[1], tokens[2], Integer.parseInt(tokens[3]));
 			}
-			else if(keyword.equals("purchase")){
-				
+			else if(tokens[0].equals("cancel")){
+				returnString = Server.cancel(Integer.parseInt(tokens[1]));
 			}
-			else if(keyword.equals("cancel")){
-				
+			else if(tokens[0].equals("search")){
+				returnString = Server.search(tokens[1]);
 			}
-			else if(keyword.equals("search")){
-				
+			else if(tokens[0].equals("list")){
+				returnString = Server.list();
 			}
-			else if(keyword.equals("list")){
-				
-			}
+			print.append(returnString);
+			Socket returnSocket = new Socket(client.getInetAddress(), client.getPort());
+			Server.tcpSend(returnSocket); 
 			client.close();
 		} catch (IOException e){
 			System.err.println(e);
-		} finally {
-			sc1.close();
-		}
+		} 
 	}
 
 }
